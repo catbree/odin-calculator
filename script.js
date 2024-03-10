1d9307f2c574b7ecfd8fca88596fa781d092bc36
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-03-10 16:52:24 +0800

    simple calculator display on html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
+const operate = (a,b,operator) => {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+};
